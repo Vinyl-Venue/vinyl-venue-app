b77c226637ae1372f0c19a0cd2fce58db239cd78
Jonathan Tucci <jonathan.tucci7@gmail.com>
2026-07-23 09:57:55 -0400

    Initial React app: header component matching design system

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header>
+        <div className="nav-bar">
+          <h1>Vinyl Venue</h1>
+          <nav className="nav-links">
+            <a href="#">Sign in</a>
+            <a href="#">Sign up</a>
+          </nav>
+        </div>
+        <p>Your collection. Your marketplace. Your bandmates.</p>
+      </header>
+    </>
+  )
+}
+
+export default App
